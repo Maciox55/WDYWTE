bd827dbed83ab8d14aae97e9d4df7722a638e36e
Maciej Bregisz <maciox55@gmail.com>
2018-06-19 02:05:22 -0400

    Accidentally leaked API keys, all keys have been invalidated.

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 module.exports={
-    cookieSecret:'y.HE:r9`5fz`Yk5.@MWBf^ak_S}dPF=*',
-    placesAPIKey:'AIzaSyCjz7hc_HwKWqNY5NbjT5Juc92FQUBnqV8'
+    cookieSecret:'COOKIE SECRET GOES HERE',
+    placesAPIKey:'GOOGLE PLACES API KEY GOES HERE'
 
 };
