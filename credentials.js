6fc7ae3887d10237c8c99b536572dd570f461257
Maciej Bregisz <maciox55@gmail.com>
2018-06-19 02:53:13 -0400

    make it better

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 module.exports={
-    cookieSecret:'y.HE:r9`5fz`Yk5.@MWBf^ak_S}dPF=*',
-    placesAPIKey:'AIzaSyBVXcgzrDjE1tnRy5cBR5lCJQi44aq52hg'
+    cookieSecret:'Dm8)L!qdg70M+mYf|)N_:~^GuGOPN*',
+    placesAPIKey:'AIzaSyBM_dsye9ZRblwQerFb_5ArD-YUrPhDGm0'
 
 };
